90c7148e8f364f7e1b24875d5a6751c54b890fd4
birk <besw05@gmail.com>
2022-04-06 10:16:13 +0200

    variabler

---
+++ BirkVariabler.js
@@ -0,0 +1,39 @@
+//Oppgave1
+//Hva betyr det å deklarere en variabel?
+//Det er å få variabelen til å få en plass i RAM. og du må gi den navn.
+//Skriv et eksempel på en deklarasjon av en variabel
+let bob;
+
+//Oppgave2
+//Hva betyr det å initialisere en variabel?
+//Å gi den en verdi
+//Skriv et eksempel der du intialiserer en variabel
+bob=20;
+
+//Oppgave3
+//Skriv et eksempel der du deklarerer OG initialiserer en variabel i samme setning.
+let bob=20;
+
+
+//Oppgave4
+//Forklar forskjellen på ‘let’ og ‘var’. Hvilken av disse bør du bruke? Forklar så godt du kan hvorfor.
+//Let kan brukes til alt inni ett scope. du kan ikke skrive let i ett scope også skrive console.log uttafor scopet. det vil da ikke fungere.
+//Var kan bara brukes i funksjoner. den er da funksjon scopa.
+
+
+
+//Oppgave5
+//Forklar forskjellen på en konstant og en variabel.
+// konst endrer seg aldri og variabel kan edre seg.
+
+//Oppgave6
+//Skriv hvilke datatyper som finnes i JavaScript. For ordens skyld. Vi har ikke gått gjennom alle, men du burde kunne navngi minst tre typer.
+
+
+//Oppgave7
+//Rett oppgaven i henhold til retteskjema på neste side
+
+
+//Oppgave8
+//Oppgaven skal lagres som PDF og levers med følgende filnavn [ditt_navn]_variabler.
+
